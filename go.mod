2ab06b710cafca0113cde28351a777465a098bcb
xiaogang.du <xiaogang.du@funplus.com>
2023-03-07 11:25:42 +0800

    ...

--- go.mod
+++ go.mod
@@ -8,7 +8,11 @@ require (
 	github.com/agiledragon/gomonkey/v2 v2.9.0
 	github.com/golang/mock v1.6.0
 	github.com/google/wire v0.5.0
+	github.com/modern-go/gls v0.0.0-20220109145502-612d0167dce5
 	github.com/vmihailenco/msgpack/v5 v5.3.5
 )
 
-require github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+require (
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+)
--- performance/gc/stw/main.go
+++ performance/gc/stw/main.go
@@ -5,20 +5,22 @@ import (
 	"time"
 )
 
-func tick(now, prev time.Time) {
+func tick(start, prev, now time.Time) {
 	//log.Printf("tick, now=%f, t=%f\n", float64(time.Now().UnixMilli())/1000, float64(t.UnixMilli())/1000)
-	elapsed := now.Sub(prev)
-	log.Printf("tick, elapsed=%v\n", elapsed)
+	e0 := now.Sub(start)
+	e1 := now.Sub(prev)
+	log.Printf("tick, e0=%v, e1=%v\n", e0, e1)
 }
 
 func main() {
 	ch := time.Tick(10 * time.Millisecond)
-	prev := time.Now()
+	start := time.Now()
+	prev := start
 	for {
 		select {
 		case <-ch:
 			now := time.Now()
-			tick(now, prev)
+			tick(start, prev, now)
 			prev = now
 		}
 	}
