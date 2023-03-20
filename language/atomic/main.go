2a81d1a6bd5f4f732542a9ab8d3aae9ea9d3206f
xiaogang.du <xiaogang.du@funplus.com>
2023-03-20 10:47:12 +0800

    update

--- language/atomic/main.go
+++ language/atomic/main.go
@@ -17,4 +17,10 @@ func main() {
 	i64 += 1 //no lock
 
 	fmt.Printf("i64=%v, a64=%v\n", i64, a64)
+
+	i := i64
+	fmt.Printf("i=%v\n", i)
+
+	i = a64.Load() //这里只有一次访存，所以不需要lock，和读取全局变量是一样的
+	fmt.Printf("i=%v\n", i)
 }
