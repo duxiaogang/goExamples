d6c6cb12e89cd64b976171d89e48f80f0a02ea19
xiaogang.du <xiaogang.du@funplus.com>
2023-03-28 14:17:48 +0800

    update

--- performance/cpu/outOfOrder/main.go
+++ performance/cpu/outOfOrder/main.go
@@ -5,14 +5,23 @@ import (
 	"sync"
 )
 
+const N = 64
+
+var arr [N]int
+
 var a, b, x, y int
 
 func f1(wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	a = 1
+
 	//var i int64
 	//atomic.AddInt64(&i, 1)
+	for i := 0; i < N; i++ {
+		arr[i]++
+	}
+
 	x = b
 }
 
@@ -20,8 +29,13 @@ func f2(wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	b = 1
+
 	//var i int64
 	//atomic.AddInt64(&i, 1)
+	for i := 0; i < N; i++ {
+		arr[i]++
+	}
+
 	y = a
 }
 
@@ -43,6 +57,9 @@ func main() {
 		wg.Wait()
 		if x == 0 && y == 0 {
 			fmt.Printf("x==y==0! i=%v, a=%v, b=%v\n", i, a, b)
+		}
+		if i%1000000 == 0 {
+			fmt.Printf("i=%v\n", i)
 		}
 	}
 }
