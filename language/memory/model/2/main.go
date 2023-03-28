e15e1377bb324143e82f307c04bc4371275c872a
xiaogang.du <xiaogang.du@funplus.com>
2023-03-28 09:57:55 +0800

    update

--- language/memory/model/2/main.go
+++ language/memory/model/2/main.go
@@ -8,47 +8,50 @@ import (
 
 const N = 100 * 1000 * 1000
 
-func main() {
-	var arrI [N]int64
-	var arrJ [N]int64
-	var arrK [N]int64
-	var flags [N]bool
+var arrI [N]int64
+var arrJ [N]int64
+var arrK [N]int64
+var flags [N]bool
 
+func f1(wg *sync.WaitGroup, index int) {
+	for i := 0; i < N; i++ {
+		arrI[i] = 1
+		arrJ[i] = 2
+		arrK[i] = 3
+		flags[i] = true
+	}
+	wg.Done()
+	fmt.Printf("%d done\n", index)
+}
+
+func f2(wg *sync.WaitGroup, index int) {
+	for i := 0; i < N; {
+		for {
+			if flags[i] {
+				if arrI[i]+arrJ[i]+arrK[i] != 6 {
+					fmt.Printf("%d error\n", index)
+				}
+				break
+			}
+		}
+		i++
+	}
+	wg.Done()
+	fmt.Printf("%d done\n", index)
+}
+
+func main() {
 	wg := sync.WaitGroup{}
 
 	bt := time.Now()
 
 	wg.Add(1)
-	go func(index int) {
-		for i := 0; i < N; i++ {
-			arrI[i] = 1
-			arrJ[i] = 2
-			arrK[i] = 3
-			flags[i] = true
-		}
-		wg.Done()
-		fmt.Printf("%d done\n", index)
-	}(1)
+	go f1(&wg, 1)
 
-	f := func(index int) {
-		for i := 0; i < N; {
-			for {
-				if flags[i] {
-					if arrI[i]+arrJ[i]+arrK[i] != 6 {
-						fmt.Printf("%d error\n", index)
-					}
-					break
-				}
-			}
-			i++
-		}
-		wg.Done()
-		fmt.Printf("%d done\n", index)
-	}
 	wg.Add(3)
-	go f(2)
-	go f(3)
-	go f(4)
+	go f2(&wg, 2)
+	go f2(&wg, 3)
+	go f2(&wg, 4)
 
 	wg.Wait()
 
