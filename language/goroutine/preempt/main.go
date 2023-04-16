1835c8d81ccc38fde2ecfc8c45c7796fc542f41c
xiaogang.du <xiaogang.du@funplus.com>
2023-04-16 15:59:06 +0800

    preempt for linux/go1.19.5

--- language/goroutine/preempt/main.go
+++ language/goroutine/preempt/main.go
@@ -2,12 +2,52 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
-var data [110]int64
+var data [101]int64
 
-func cpu100(index int) {
+func makeStackBigger(size int) {
+	if size == 0 {
+		return
+	}
+	makeStackBigger(size-1)
+}
+
+func worker(index int) {
+	makeStackBigger(1000)
+	for {
+		runtime.ResetInfloopChecker(100)
+		if index < 97 {
+			normal(index)
+		} else {
+			infLoop(index)
+		}
+	}
+}
+
+func normal(index int) {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Printf("normal(%d), e=%v\n", index, e)
+		}
+	}()
+
+	data[0]++
+	for i := 0; i < 1000*1000*1000; i++ {
+		data[index]++
+	}
+}
+
+func infLoop(index int) {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Printf("infLoop(%d), e=%v\n", index, e)
+		}
+	}()
+
+	data[0]++
 	for {
 		data[index]++
 	}
@@ -15,7 +55,7 @@ func cpu100(index int) {
 
 func main() {
 	for i := 0; i < 100; i++ {
-		go cpu100(i)
+		go worker(i)
 	}
 
 	ch := time.Tick(1 * time.Second)
