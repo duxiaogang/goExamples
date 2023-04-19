6a965ca13115726ed4159df95eedab60ba079dff
xiaogang.du <xiaogang.du@funplus.com>
2023-04-19 18:29:42 +0800

    array/d1

--- performance/array/d1/main.go
+++ performance/array/d1/main.go
@@ -5,26 +5,26 @@ import (
 	"math/rand"
 )
 
-const SIZE = 1000
-const CYCLE = 1000 * 1000
+const SIZE = 1000 * 1000 * 1000
+const RANGE = 1 * 1000 * 1000
+const COUNT = 1 * 1000 * 1000
 
 func main() {
-	size := SIZE * SIZE
-	data := make([]int, size)
-	for i := 0; i < size; i++ {
-		data[i] = rand.Intn(100)
+	data := make([]int, SIZE)
+	for i := 0; i < SIZE; i++ {
+		data[i] = rand.Int()
 	}
-	xSlice := make([]int, CYCLE)
-	ySlice := make([]int, CYCLE)
-	for j := 0; j < CYCLE; j++ {
-		tmp := rand.Intn(size)
-		xSlice[j] = tmp / SIZE
-		ySlice[j] = tmp % SIZE
+	xSlice := make([]int, COUNT)
+	ySlice := make([]int, COUNT)
+	for i := 0; i < COUNT; i++ {
+		tmp := rand.Intn(RANGE)
+		xSlice[i] = tmp
+		ySlice[i] = 0
 	}
 
 	sum := 0
-	for i := 0; i < 1000*1; i++ {
-		for j := 0; j < CYCLE; j++ {
+	for i := 0; i < 1*1000; i++ {
+		for j := 0; j < COUNT; j++ {
 			sum += data[xSlice[j]] + ySlice[j]
 		}
 	}
